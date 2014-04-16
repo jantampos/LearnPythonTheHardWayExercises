f2f2891dd5f5c97e20f2ded61cb1d18bb5eeb395
Jan Tamps <janaubrey.tampos@gmail.com>
2014-04-16 10:58:45 +0800

    First Commit First Added File

---
+++ ex13.py
@@ -0,0 +1,11 @@
+from sys import argv
+
+script, first, second, third = argv
+
+print "The script is called: ", script
+print "Your 1st variables is: ", first
+print "Your second variables is: ", second
+print "Your third variables is: ", third
+
+
+raw_input("Are you having fun?")
